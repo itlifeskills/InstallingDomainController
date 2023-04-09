f0393020fcf37e7a5425a6153eef9a7ed6865e83
itlifeskills <106455363+itlifeskills@users.noreply.github.com>
2023-04-09 12:01:26 -0500

    Update DisableAdminAutoLogon.ps1

--- DisableAdminAutoLogon.ps1
+++ DisableAdminAutoLogon.ps1
@@ -1,5 +1,8 @@
-﻿$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 $Name = "AutoAdminLogon"
+$User = "DefaultUserName"
 $value = "0"
 Set-ItemProperty -Path $registryPath -Name $name -Value $value -Force | Out-Null
+Remove-ItemProperty -Path $registryPath -Name $User
+
 
