548c84c8c7beeb0f85b6e3b12e60c89e8df8a87e
itlifeskills <106455363+itlifeskills@users.noreply.github.com>
2023-04-09 11:45:15 -0500

    Update DisableIPv6.ps1

--- DisableIPv6.ps1
+++ DisableIPv6.ps1
@@ -1,5 +1,5 @@
 ﻿#Get all network adapter bindings enabled with IPv6 then disable them
 
-Get-NetAdapterBinding -ComponentID ms_tcpip6 | Select Enabled
+Get-NetAdapterBinding -ComponentID ms_tcpip6 |
 Where-Object {$_.Enabled -eq $true} | 
 Disable-NetAdapterBinding  -ComponentID ms_tcpip6
