60b27616572a9065298db7c042421df86f280ae5
itlifeskills <106455363+itlifeskills@users.noreply.github.com>
2023-04-09 20:45:38 -0500

    Update DisableAdminAutoLogon.ps1

--- DisableAdminAutoLogon.ps1
+++ DisableAdminAutoLogon.ps1
@@ -1,8 +1,7 @@
 $registryPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 $Name = "AutoAdminLogon"
-$DefaultPassword = "DisableAdminAutoLogon"
 $value = "0"
 Set-ItemProperty -Path $registryPath -Name $name -Value $value -Force | Out-Null
-Set-ItemProperty $RegPath "DefaultPassword" -Value $DefaultPassword -type String
+
 
 
